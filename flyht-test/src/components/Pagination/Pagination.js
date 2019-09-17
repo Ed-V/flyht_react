8e1c947d005d92762f7458fdc8590abe66b30a16
Edbert Voo <developer@edbertvoo.com>
2019-09-16 22:52:07 -0600

    Basic button selection implemented

--- flyht-test/src/components/Pagination/Pagination.js
+++ flyht-test/src/components/Pagination/Pagination.js
@@ -1,5 +1,42 @@
+import React from "react";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+
 const pagination = props => {
-<p></p>
+  return (
+    <div>
+      <ButtonGroup className="flex-wrap">
+        {genButtons(props)}
+      </ButtonGroup>
+    </div>
+  );
+};
+
+function genButtons(props){
+let result=[];
+for (let index = 1; index <= calcTotals(props); index++) {
+
+if(props.selectedButton == index){
+    result.push(<Button variant="secondary" key={index} data-bindex={index} onClick={props.paginationButtonClickHandler}>{index}</Button>);
+} else {
+    result.push(<Button variant="primary" key={index} data-bindex={index} onClick={props.paginationButtonClickHandler}>{index}</Button>);
+}
+
+
+}
+return result;
+}
+
+function calcTotals(props) {
+  let result = Math.ceil(props.studentTotal / props.limit);
+
+  if (result === 0) {
+    result = 1;
+  }
+
+result = 10;
+
+  return result;
 }
 
 export default pagination;
--- flyht-test/src/components/StudentManager/StudentManager.js
+++ flyht-test/src/components/StudentManager/StudentManager.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React from "react";
+import Axios from "../../utils/AxiosWrap";
+import Pagination from "../Pagination/Pagination";
+import Container from "react-bootstrap/Container";
 
 class StudentManager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { studentTotal: 0, limit: 10, selectedButton: 1 };
+  }
 
-    render(){
-        return <p>Hello world</p>;
-    }
+  componentDidMount() {
+    this.fetchTotalStudents();
+  }
+
+  fetchTotalStudents() {
+    Axios.get("students?totals=true&count=true")
+      .then(response => {
+        console.log(response);
+        this.setState({ studentTotal: response.data.totals.count });
+      })
+      .catch(error => {
+        alert("An error occured, see console for more details");
+        console.log(error);
+      });
+  }
+
+  paginationButtonClickHandler = (event) => {
+    let buttonIndex = event.target.dataset.bindex;
+    //console.log(this);
+    this.setState({ selectedButton:  buttonIndex});
+  }
+
+  render() {
+    return (
+      <Container>
+        <Pagination
+          studentTotal={this.state.studentTotal}
+          limit={this.state.limit}
+          selectedButton={this.state.selectedButton}
+          paginationButtonClickHandler={this.paginationButtonClickHandler}
+        ></Pagination>
+      </Container>
+    );
+  }
 }
 
 export default StudentManager;
