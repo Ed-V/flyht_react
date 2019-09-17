2670004487b306d59fbee4a23f3a0298c1a8c742
Edbert Voo <developer@edbertvoo.com>
2019-09-17 00:00:05 -0600

    stored list in to mobx

--- flyht-test/src/components/StudentManager/StudentManager.js
+++ flyht-test/src/components/StudentManager/StudentManager.js
@@ -2,16 +2,37 @@ import React from "react";
 import Axios from "../../utils/AxiosWrap";
 import Pagination from "../Pagination/Pagination";
 import Container from "react-bootstrap/Container";
+import {inject, observer} from 'mobx-react'
 
+@inject('StudentStore')
+@observer
 class StudentManager extends React.Component {
   constructor(props) {
     super(props);
     this.state = { studentTotal: 0, limit: 10, selectedButton: 1 };
   }
 
+
   componentDidMount() {
     this.fetchTotalStudents();
+    this.fetchStudents();
   }
+
+
+fetchStudents(){
+    Axios.get("students", {params: {
+        max: this.state.limit
+    }})
+    .then(response => {
+      console.log(response);
+      this.props.StudentStore.setStudents(response.data);
+      console.log(this.props.StudentStore.students);
+    })
+    .catch(error => {
+      alert("An error occured, see console for more details");
+      console.log(error);
+    });
+}
 
   fetchTotalStudents() {
     Axios.get("students?totals=true&count=true")
@@ -32,6 +53,10 @@ class StudentManager extends React.Component {
   }
 
   render() {
+
+    const {StudentStore} = this.props;
+
+
     return (
       <Container>
         <Pagination
