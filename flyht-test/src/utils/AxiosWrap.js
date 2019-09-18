d197f8d637de16f70753f1ebc2a0271af1910bd7
Edbert Voo <developer@edbertvoo.com>
2019-09-17 20:43:39 -0600

    Updates

--- flyht-test/src/utils/AxiosWrap.js
+++
@@ -1,6 +0,0 @@
-import Axios from 'axios'
-
-Axios.defaults.baseURL = "https://flyht-354b.restdb.io/rest/";
-Axios.defaults.headers.common['x-apikey'] = "5d7fa78ffd86cb75861e2350";
-
-export default Axios;
